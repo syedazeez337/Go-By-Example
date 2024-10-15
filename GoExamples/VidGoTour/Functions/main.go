e06c010587a4ff6b7791bc08cbd891667673bcd9
Syed Azeez <syedazeez337@gmail.com>
2024-10-15 11:56:28 +0530

    new

--- GoExamples/VidGoTour/Functions/main.go
+++ GoExamples/VidGoTour/Functions/main.go
@@ -19,6 +19,16 @@ func mulDiv(a int, b int) (int, int) {
 	return a * b, a / b
 }
 
+// Variadic function
+func sumofNums(nums ...int) int {
+	sum := 0
+
+	for _, num := range nums {
+		sum += num
+	}
+	return sum
+}
+
 func main() {
 	hello()
 	a, b := 4, 2
@@ -26,4 +36,12 @@ func main() {
 
 	mul, div := mulDiv(a, b)
 	fmt.Printf("Mul(%d,%d) -> %d, Div(%d, %d) -> %d\n", a, b, mul, a, b, div)
+
+	twoSum := sumofNums(1,2)
+	fmt.Printf("Sum of two: %d\n", twoSum)
+	threeSum := sumofNums(1,2,3,4,5)
+	fmt.Printf("Sum of three: %d\n", threeSum)
+	arr := []int {1,2,3,4,5}
+	sliceSum := sumofNums(arr...)
+	fmt.Printf("Slice sum: %d\n", sliceSum)
 }
