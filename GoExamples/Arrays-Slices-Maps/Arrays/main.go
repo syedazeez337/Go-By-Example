038725f581a35cf5d8d2d36cd5118f119a6017cd
Syed Azeez <syedazeez337@gmail.com>
2024-10-24 11:53:16 +0530

    New lines

--- GoExamples/Arrays-Slices-Maps/Arrays/main.go
+++ GoExamples/Arrays-Slices-Maps/Arrays/main.go
@@ -6,6 +6,28 @@ import (
 
 func main() {
 
+	var a [4]int
+
+	a[0] = 1
+	a[1] = 2
+	a[2] = 3
+	a[3] = 4
+
+	fmt.Println("Array>", a)
+
+	b := [5]int{21,22,32,45,67}
+	fmt.Println("Array 2>", b)
+	
+	// slice declaration
+	c := []string {"a","b","c","d"}
+	fmt.Println("Slice>", c)
+
+	d := make([]int, 5)
+	fmt.Println("Empty slice>", d)
+	d = append(d, 2)
+	d = append(d, 3)
+	fmt.Println("Refined slice>", d)
+
 	phoneBook := map[string]int{
 		"A": 123,
 		"C": 456,
